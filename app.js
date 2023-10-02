a754b61476d5a6e6d5942f71f6b21f3aec39a75a
j-greig <3stripe@gmail.com>
2023-10-02 14:49:44 +0100

    Check user is Insomnia Live / Live Pro subscriber before giving them a login link

--- app.js
+++ app.js
@@ -29,49 +29,38 @@ const secretKey = process.env.JWT_SECRET_KEY;
 app.use(express.json());
 
 // Define a route to handle incoming POST requests to /login
-app.post('/login', (req, res) => {
+app.post('/login', async (req, res) => {
     // Parse and log the incoming data payload
     console.log('Received data:');
     console.log(req.body);
 
-    const textAfterSnoop = req.body.message.text.replace(/^\/login\s+/, '') ?? 'n/a';
     const chatId = req.body.message.chat.id ?? 'n/a';
     const userId = req.body.message.from.id;
     const username = req.body.message.from.username;
-    console.log(`chat id: ${chatId}, userid: ${userId}, username: ${username}`);
+    console.log(`Insomnia Live login request received\nchat id: ${chatId}, userid: ${userId}, username: ${username}`);
 
-    // Send login link to paid subscriber :)
-    const token = jwt.sign({ username: req.body.message.from.username }, secretKey);
-    const options = { disable_web_page_preview: true };
-    accessBot.sendMessage(chatId, `Here's the login link you requested:\nhttps://www.insomniahq.xyz/autologin/?auth=${token}`, options)
-        .catch(err => {
-            console.error('Error sending message:', err);
-        });
+    const apiKey = process.env.TGMEMBERSHIP_API_KEY;
+    const apiUrl = `https://api.tgmembership.com/bot6448465749/${apiKey}/getSubscribers?user_id=${userId}`;
 
-    // Respond with a 200 status code to acknowledge receipt of the payload
-    res.sendStatus(200);
-});
+    try {
+        const response = await axios.get(apiUrl);
+        const { plan_id } = response.data.result;
 
-app.post('/verify', (req, res) => {
-
-    console.log('Received a request to /verify');
-    const token = req.body.token;
-
-    jwt.verify(token, secretKey, (err, decoded) => {
-        if (err) {
-            return res.status(401).json({ message: 'Token verification failed' });
+        if (plan_id === 2 || plan_id === 3) {
+            // Send login link to paid subscriber :)
+            const token = jwt.sign({ username }, secretKey);
+            const options = { disable_web_page_preview: true };
+            accessBot.sendMessage(chatId, `Here's the login link you requested:\nhttps://www.insomniahq.xyz/autologin/?auth=${token}`, options);
+        } else {
+            accessBot.sendMessage(chatId, "Sorry, it looks like you're not a paid subscriber to Insomnia Live.");
         }
-        // Send back the decoded information
-        res.json(decoded);
-        // Log 
-        console.log('Sucessfully decoded token');
-        // Tell the user they have logged in, lolz
-        // const options = { disable_web_page_preview: true };
-        // accessBot.sendMessage(chatId, `👋 Login to your Shitcoin HQ account detected. Hopefully this was you - if not, nae luck pal! 💩`, options)
-        //     .catch(err => {
-        //         console.error('Error sending message:', err);
-        //     });
-    });
+
+    } catch (err) {
+        console.error('API call or something else went wrong:', err);
+        accessBot.sendMessage(chatId, "Sorry, an error occurred while checking your subscription. Please wait a minute and try again.");
+    }
+
+    res.sendStatus(200);
 });
 
 // Start the HTTP server and listen on the specified port
